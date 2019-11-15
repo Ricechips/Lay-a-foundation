b06c8b602cc9c730b60e3c7053675f2844ceaa15
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-15 10:59:59 +0800

    Create loop.php

---
+++ loop.php
@@ -0,0 +1,27 @@
+<?php
+
+for($i=0;$i<100;$i++){
+  echo 'hello '.$i.'<br>;
+  
+//  if($i==20){
+//    break;
+//  }
+    
+    if($i==20){
+      continue;
+    }
+    
+    echo 'homie'.$i.'<br>';
+}
+
+//$i = 0;
+//while($i<10){
+//  echo 'hello '.$i.'<br>';
+//  $i++;
+//}
+
+//$i = 0;
+//do{
+//   echo 'hello '.$i.'<br>';
+//   $i++;
+//}while($i<100);
