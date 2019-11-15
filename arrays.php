84287a36319a77c78eb720081a60084f8b713e77
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-15 11:32:20 +0800

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+$arr = array(0=>'yz',1=>'fhq','h'=>'Shiba','w'=>'Inu','name=>'Ricechips');
+
+
+//$arr['h'] = 'Shiba';
+//$arr['w'] = 'Inu';
+
+//$arr[0] = 'Shiba';
+//$arr[1] = 'Inu';
+//$arr[2] = 2;
+//$arr[3] = 3.14;
+
+//for($i=0;$i<100;$i++){
+//    array_push($arr,'Item '.$i)  //view page source
+//}
+
+print_r($arr);
+
+//echo $arr['h'];
+
+echo $arr[name];
