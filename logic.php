7c866c54c1e320d57f5ca6e8c0719008c3a63aac
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-15 11:07:26 +0800

    Create logic.php

---
+++ logic.php
@@ -0,0 +1,21 @@
+<?php
+
+function traceNum(){
+   for($i=0;$i<=100;$i++){
+//      if($i%2==0 && $i%3==0){
+//          echo $i'<br>';
+//      }
+        
+//       if($i%2==0 || $i%3==0){
+//          echo $i'<br>';
+//       }
+         
+         if($i%2!=0){
+            echo $i'<br>';
+         }
+
+
+   }
+}
+
+traceNum();
