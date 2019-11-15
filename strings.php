4af0aa45f7292b5ef9f87a5656b0cc63bea7b98c
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-15 11:22:37 +0800

    Create strings.php

---
+++ strings.php
@@ -0,0 +1,23 @@
+<?php
+
+$str = 'Hi Shiba Inu husky corgi shepherd';
+//echo $str;
+
+//echo strpos($str,'sk');
+
+$str1 = substr($str,3,4);
+echo $str1;
+
+//$result = str_split($str);
+//print_r($result);
+
+//$result = explode(' ',$str);
+//print_r($result);
+
+$num = 5;
+
+//$str2 = $str.'<br>labrador '.$num;
+//echo $str2;
+
+$str2 = "$str<br>labrador $num";
+echo $str2;
