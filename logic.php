d52dd16c0bc28f5c9a6d28f5d19edc6b6ff2714b
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-19 09:24:08 +0800

    Update logic.php

--- logic.php
+++ logic.php
@@ -3,15 +3,15 @@
 function traceNum(){
    for($i=0;$i<=100;$i++){
 //      if($i%2==0 && $i%3==0){
-//          echo $i'<br>';
+//          echo $i.'<br>';
 //      }
         
 //       if($i%2==0 || $i%3==0){
-//          echo $i'<br>';
+//          echo $i.'<br>';
 //       }
          
          if($i%2!=0){
-            echo $i'<br>';
+            echo $i.'<br>';
          }
 
 
